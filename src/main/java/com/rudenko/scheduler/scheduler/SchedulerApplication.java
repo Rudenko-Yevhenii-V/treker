adc6a746a0249ba1846da719b9265c4da6d94c32
Rudenko Yevhenii <heltyrjvbi@gmail.com>
2022-07-09 21:06:06 +0300

    firs ver 1

--- src/main/java/com/rudenko/scheduler/scheduler/SchedulerApplication.java
+++ src/main/java/com/rudenko/scheduler/scheduler/SchedulerApplication.java
@@ -1,46 +1,46 @@
 package com.rudenko.scheduler.scheduler;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import java.io.*;
-import java.time.Instant;
 import java.time.LocalDate;
-import java.time.ZoneId;
 
 @SpringBootApplication
 public class SchedulerApplication {
-	public static final String FILE_NAME = "scheduler.txt";
+	public static final String FILE_NAME = "_scheduler.txt";
 	public static final int MINUTE = 60000;
 	public static String fileName;
+
+	@Value("${time}")
+	public String time;
+	private static String TIME_STATIC;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SchedulerApplication.class, args);
 		try {
+			System.out.println("TIME_STATIC == > " + TIME_STATIC);
 			Thread.sleep(MINUTE * 2);
-
 			fileName = LocalDate.now() + FILE_NAME;
 
 			if (!new File(fileName).exists()) {
 				createAndSave();
 			}
-			String read = read();
 
 			int dalay = MINUTE * 10;
 			boolean life = true;
 
-			if (!(Instant.parse(read).atZone(ZoneId.systemDefault()).getDayOfWeek()
-					.equals(Instant.now().atZone(ZoneId.systemDefault()).getDayOfWeek())
-			)) {
-				createAndSave();
-			}
-
 			while (life) {
+				String read = read();
 				String[] times = read.split(":");
 				int timeTo = Integer.parseInt(times[0]);
 				int timeNow = Integer.parseInt(times[1]);
 
 				if (timeTo > timeNow) {
-					save(String.format("%s:%s", timeTo, timeNow + dalay));
+					String timeToWriteFormated = String.format("%s:%s", timeTo, timeNow + dalay);
+					System.out.println("timeToWriteFormated = " + timeToWriteFormated);
+					save(timeToWriteFormated);
 					Thread.sleep(dalay);
 				} else {
 					life = false;
@@ -52,6 +52,10 @@ public class SchedulerApplication {
 		}
 	}
 
+	@Value("${time}")
+	public void setNameStatic(String time){
+		TIME_STATIC = time;
+	}
 
 	private static String read() throws IOException {
 		BufferedReader reader = new BufferedReader(new FileReader(fileName));
@@ -65,8 +69,10 @@ public class SchedulerApplication {
 	}
 
 	private static void createAndSave() throws IOException {
+		int hours = Integer.parseInt(TIME_STATIC);
 		FileWriter writer = new FileWriter(fileName);
-		writer.write(String.format("%s:%s", (MINUTE * 60 * 3), 0));
+		writer.write(String.format("%s:%s", (MINUTE * 60 * hours), 0));
 		writer.close();
 	}
+
 }
